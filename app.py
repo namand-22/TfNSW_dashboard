c7b7e02a59fce930499453a1a76669ec01eac423
namand-22 <namandodia1@gmail.com>
2025-10-07 09:34:45 +1100

    added check_departures function to flask code

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from main import check_departures
+from main import check_departures
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<html><p>Hello, World</p></html>"
+    return check_departures()
 
 if __name__ == "__main__":
-    check_departures()
     app.run(host="0.0.0.0", port=80)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import datetime
 
+# accessing the API
 api_key = "wHYh-AIObxYyTV0PCY3R0huV0nOXG1pCmAAWm8GZxXk"
 base_url = "https://api.transport.nsw.gov.au/v1"
 departure_url = "/tp/departure_mon"
@@ -8,7 +9,7 @@ full_url = base_url + departure_url
 
 # utc is 11 hrs behind aest
 timezone_offset = 39600
-    
+   
 # current date and time
 current_datetime = datetime.datetime.now()
 current_date = current_datetime.strftime("%Y%m%d")
@@ -21,7 +22,7 @@ def check_departures():
         "coordOutputFormat": "EPSG:4326",
         "mode": "direct",
         "type_dm": "stop",
-        "name_dm": "2154392",  # Hills Showground stop ID
+        "name_dm": "2154392",
         "depArrMacro": "dep",
         "itdDate": current_date,
         "itdTime": current_time,
@@ -32,22 +33,35 @@ def check_departures():
         "Authorization": "apikey " + api_key
     }
 
+    # requesting data
     response = requests.get(full_url, headers=headers, params=api_parameters)
     data = response.json()
     stop_events = data["stopEvents"]
 
+    # empty retrun variable to be added to 
+    upcoming_departures = ""
+
+    # checks for upcoming departures from Hills Showground
     for stop_event in stop_events[:20]:
         platform_number = stop_event["location"]["properties"]["platformName"]
         
+        # if upcoming departures are from the metro, not bus
         if platform_number == "Platform 1" or platform_number == "Platform 2":
             final_destination = stop_event["transportation"]["destination"]["name"]
             departure_time = stop_event["departureTimePlanned"]
             
             datetime_format_departure_time = datetime.datetime.strptime(departure_time, "%Y-%m-%dT%H:%M:%SZ")    
 
+            # calculate the difference in current time and time till departure
             seconds_till_departure = (datetime_format_departure_time.timestamp() - current_datetime.timestamp() + timezone_offset)
             minutes_till_departure = round(seconds_till_departure / 60)
 
-            print(f"The next train to arrive on {platform_number} ", end="") 
-            print(f"goes to {final_destination}, ", end="") 
-            print(f"departing in {minutes_till_departure} minutes")
+            # create a string of the upcoming departures
+            upcoming_departures += (f"The next train to arrive on {platform_number} goes to {final_destination}, departing in {minutes_till_departure} minutes<br>")
+
+    # return to flask program
+    return upcoming_departures
+
+
+if __name__ == "__main__":
+    print(check_departures())
